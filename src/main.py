c34d327be7701ce2272474630076d9b40ad7bb8f
Angel Ramirez <angelra@gmail.com>
2024-10-06 22:22:14 -0700

    added initial unit tests

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextNode
+
+def main():
+    node = TextNode("This is a text node", "bold", "https://boot.dev")
+    print(node)
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+        
+    def test_repr(self):
+        node = TextNode("This is a text node", "bold", "http://0.0.0.0:8888")
+        assert repr(node) == "TextNode(This is a text node, bold, http://0.0.0.0:8888)"
+        
+    def test_url_default_value(self):
+        node = TextNode("This is a text node", "bold")
+        assert node.url == None
+        
+    def test_not_equal_properties(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "header")
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
