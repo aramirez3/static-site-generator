526de66190aebec55ccfbc37af80f67fdd46f579
Angel Ramirez <angelra@gmail.com>
2024-10-07 12:59:44 -0700

    Test for ParentNode.children = required

--- src/parentnode.py
+++ src/parentnode.py
@@ -3,3 +3,8 @@ from htmlnode import HTMLNode
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props):
         super().__init__(tag, None, children, props)
+        
+    def to_html(self):
+        if self.children is None or self.children == []:
+            raise ValueError("Parent.children is required")
+        
--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -4,8 +4,14 @@ from parentnode import ParentNode
 
 class TestParentNode(unittest.TestCase):
     def test_default_values(self):
-        node = ParentNode('p', [], {"class":"all-of-them"})
+        node = ParentNode('p', ['a'], {"class":"all-of-them"})
         self.assertEqual(node.tag, 'p')
         self.assertEqual(node.value, None)
-        self.assertEqual(node.children, [])
+        self.assertEqual(node.children, ['a'])
         self.assertEqual(node.props, {'class':'all-of-them'})
+        
+    def test_to_html_no_children(self):
+        node = ParentNode('p', [], {"class":"all-of-them"})
+        self.assertRaises(ValueError, node.to_html)
+        node2 = ParentNode('p', None, {"class":"all-of-them"})
+        self.assertRaises(ValueError, node2.to_html)
