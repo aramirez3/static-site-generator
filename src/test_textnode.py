e3e480214d8962ddd965470f460a13fc46eaa651
Angel Ramirez <angelra@gmail.com>
2024-10-08 09:26:11 -0700

    TextNode assertions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,7 +2,7 @@ from unittest.result import TestResult
 from assertionhelper import AssertionHelper
 
 from textnode import TextNode
-from utilities import text_node_to_html_node
+from utilities import *
 
 
 class TestTextNode(AssertionHelper):
@@ -26,40 +26,50 @@ class TestTextNode(AssertionHelper):
         
     def test_text_types(self):
         list_of_types = [
-            {"text":"text type", "text_type": "text", "url": None, "expected": {
+            {"text":"text type", "text_type": TextTypes.TEXT, "url": None, "expected": {
                 "tag": "", "value": "text type", "props": None
             }},
-            {"text":"bold type", "text_type": "bold", "url": None, "expected": {
+            {"text":"bold type", "text_type": TextTypes.BOLD, "url": None, "expected": {
                 "tag": "b", "value": "bold type", "props": None
             }},
-            {"text":"italic type", "text_type": "italic", "url": None, "expected": {
+            {"text":"italic type", "text_type": TextTypes.ITALIC, "url": None, "expected": {
                 "tag": "i", "value": "italic type", "props": None
             }},
-            {"text":"code type", "text_type": "code", "url": None, "expected": {
+            {"text":"code type", "text_type": TextTypes.CODE, "url": None, "expected": {
                 "tag": "code", "value": "code type", "props": None
             }},
-            {"text":"link type", "text_type": "link", "url": "/home", "expected": {
+            {"text":"link type", "text_type": TextTypes.LINK, "url": "/home", "expected": {
                 "tag": "a", "value": "link type", "props": {'href': '/home'}
             }},
-            {"text":"image type", "text_type": "image", "url": "/images/doggie.jpg", "expected": {
+            {"text":"image type", "text_type": TextTypes.IMAGE, "url": "/images/doggie.jpg", "expected": {
                 "tag": "img", "value": "", "props": {'src': '/images/doggie.jpg', 'alt': 'image type'}
             }}
         ]
         for item in list_of_types:
-            text = TextNode(item["text"], item["text_type"], item["url"])
-            leaf = text_node_to_html_node(text)
-            self.text_type_conversion_assertions(leaf, item["expected"]["tag"], item["expected"]["value"], item["expected"]["props"])
+            text_node = TextNode(item["text"], item["text_type"], item["url"])
+            self.text_type_conversion_assertions(text_node, item["expected"]["tag"], item["expected"]["value"], item["expected"]["props"])
     
     def test_text_types_not_valid(self):
         node = TextNode("invalid type", "boomerang")
+        print(f"\nnode: {node}")
         self.assert_exception_and_message(text_node_to_html_node, ValueError, "Not a valid text type", node)
     
-    def text_type_conversion_assertions(self, node, tag, value, props = None):
-        print(f'node being tested: {node}')
-        self.assertEqual(type(node).__name__, "LeafNode")
-        self.assertEqual(node.tag, tag)
-        self.assertEqual(node.value, value)
-        self.assertEqual(node.props, props)
+    def text_type_conversion_assertions(self, text_node, tag, value, props = None):
+        leaf_node = text_node_to_html_node(text_node)
+        self.assertEqual(type(leaf_node).__name__, "LeafNode")
+        self.assertEqual(leaf_node.tag, tag)
+        self.assertEqual(leaf_node.value, value)
+        self.assertEqual(leaf_node.props, props)
         
+    def test_raw_text_to_nodes(self):
+        node = TextNode("This is text with a `code block` word", TextTypes.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextTypes.TEXT)
+        expected_nodes = [
+            TextNode("This is text with a ", TextTypes.TEXT),
+            TextNode("code block", TextTypes.CODE),
+            TextNode(" word", TextTypes.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+    
 if __name__ == "__main__":
     AssertionHelper.main()
--- src/utilities.py
+++ src/utilities.py
@@ -1,17 +1,31 @@
 from leafnode import LeafNode
+from enum import Enum
+
+class TextTypes(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 def text_node_to_html_node(text_node):
-    if text_node.text_type == "text":
+    if text_node.text_type == TextTypes.TEXT:
         return LeafNode("", text_node.text)
-    elif text_node.text_type == "bold":
+    elif text_node.text_type == TextTypes.BOLD:
         return LeafNode("b", text_node.text)
-    elif text_node.text_type == "italic":
+    elif text_node.text_type == TextTypes.ITALIC:
         return LeafNode("i", text_node.text)
-    elif text_node.text_type == "code":
+    elif text_node.text_type == TextTypes.CODE:
         return LeafNode("code", text_node.text)
-    elif text_node.text_type == "link":
+    elif text_node.text_type == TextTypes.LINK:
         return LeafNode("a", text_node.text, {"href":text_node.url})
-    elif text_node.text_type == "image":
+    elif text_node.text_type == TextTypes.IMAGE:
         return LeafNode("img", "", {"src":text_node.url, "alt":text_node.text})
     else:
+        print(f"\nnode type: {text_node.text_type}")
         raise ValueError("Not a valid text type")
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    
+    pass
