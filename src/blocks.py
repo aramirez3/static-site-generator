0c1eda58d894899e2de1d056bc577312abce4243
Angel Ramirez <angelra@gmail.com>
2024-10-09 15:28:51 -0700

    split up markdown to list of text blocks

--- src/blocks.py
+++ src/blocks.py
@@ -1,5 +1,37 @@
 from utilities import *
 
-def markdown_to_blocks(markdown):
-    # return list of block strings
-    return []
+def markdown_to_blocks(markdown_text):
+    blocks = []
+    if markdown_text == "" or markdown_text is None:
+        return markdown_text
+    
+    lines = markdown_text.split("\n")
+    
+    # probably a fancier way to do this but this works ¯\_(ツ)_/¯
+    start = 0
+    end = 0
+    prev_empty = 0
+    prev_not_empty = 0
+    inside_text_block = False
+    if lines:
+        for i in range(0, len(lines)):
+            line = lines[i]
+            # empty line is not saved
+            if line == "" or line == None:
+                prev_empty = i
+            else:
+                prev_not_empty = i
+            # check beginning of new text block
+            if prev_empty < prev_not_empty:
+                # if inside a text block then continue to the next line
+                if not inside_text_block:
+                    inside_text_block = True
+                    start = i
+            # check for end of the current text block
+            elif prev_empty > prev_not_empty:
+                # a new blank line was found so we need to snapshot the text block
+                end = i
+                block = lines[start:end]
+                blocks.append("\n".join(block))
+                inside_text_block = False
+    return blocks
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -9,21 +9,32 @@ from utilities import *
 
 class TestTextNode(AssertionHelper):
     def test_markdown_to_blocks(self):
-        markdown = '''# This is a heading
+        text = markdown_text = """
+# This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
 * This is the first list item in a list block
 * This is a list item
-* This is another list item'''
-        actual = markdown_to_blocks(markdown)
+* This is another list item
+"""
+        actual = markdown_to_blocks(text)
         expected = [
             "# This is a heading",
             "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
-'''* This is the first list item in a list block
+            """* This is the first list item in a list block
 * This is a list item
-* This is another list item'''
+* This is another list item"""
         ]
+        self.assertEqual(actual, expected)
+        
+    def test_markdown_to_blocks_empty(self):
+        actual = markdown_to_blocks("")
+        expected = ""
+        self.assertEqual(actual, expected)
+        
+        actual = markdown_to_blocks(None)
+        expected = None
         self.assertEqual(actual, expected)
         
 if __name__ == "__main__":
