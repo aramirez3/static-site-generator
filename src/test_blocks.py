b196ab9b76df9b6a0237a5185ec861cb2e56439f
Angel Ramirez <angelra@gmail.com>
2024-10-09 16:08:07 -0700

    markdown to text refactor

--- src/test_blocks.py
+++ src/test_blocks.py
@@ -30,11 +30,29 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         
     def test_markdown_to_blocks_empty(self):
         actual = markdown_to_blocks("")
-        expected = ""
+        expected = []
         self.assertEqual(actual, expected)
         
-        actual = markdown_to_blocks(None)
-        expected = None
+    def test_markdown_to_blocks_trim_whitespace(self):
+        markdown_text="""
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        expected = [
+            "This is **bolded** paragraph",
+            "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+            "* This is a list\n* with items",
+        ]
+        
+        actual = markdown_to_blocks(markdown_text)
         self.assertEqual(actual, expected)
         
 if __name__ == "__main__":
